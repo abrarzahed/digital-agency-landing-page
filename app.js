0f7d0d5337683b9c9ebd8a46ef35069a36efe460
abrarzahed <abrarzahed6986@gmail.com>
2022-04-20 02:44:03 +0600

    completed

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ window.onload = function loadFn() {
   toggleBtn.addEventListener("click", function (e) {
     collapseMenu.classList.toggle("active");
   });
+
+  // Masonry
+  const grid = document.querySelector(".images-flex");
+  const masonry = new Masonry(grid, {
+    itemSelector: ".flex-item",
+    gutter: 60,
+    columnWidth: ".flex-item",
+
+    // fitWidth: true,
+  });
 };
+
+// rellax js
+const rellax = new Rellax(".rellax", {
+  center: true,
+});
